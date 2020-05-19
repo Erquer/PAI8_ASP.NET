c744025cb7c08a471d59b5f06397bf4a77940fd5
Błażej <baryla181@gmail.com>
2020-05-19 14:09:31 +0200

    Dodaj pliki projektów.

---
+++ PAI8_1/Data/PAI8_1Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using PAI8_1.Models;
+
+namespace PAI8_1.Data
+{
+    public class PAI8_1Context : DbContext
+    {
+        public PAI8_1Context (DbContextOptions<PAI8_1Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<PAI8_1.Models.Movie> Movie { get; set; }
+    }
+}
---
+++ PAI8_1/Pages/Name.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PAI8_1.Pages
+{
+    public class NameModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
